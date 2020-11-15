ba74b7a4deee1368cd26e0e33e1c0b1fa146dfc5
tanabe <m.k.masakatsu@gmail.com>
2020-11-16 03:17:50 +0900

    method fix

--- src/controller/GenresController.ts
+++ src/controller/GenresController.ts
@@ -7,26 +7,45 @@ export class GenresController {
     private userRepository = getRepository(Genres);
 
     async all(request: Request, response: Response, next: NextFunction) {
+        const genres: Genres[] = await this.userRepository.find();
+        const status = typeof genres === "undefined" ? 400 : 200;
+        response.status(status);
+        response.json(genres);
         return this.userRepository.find();
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
+        const genres: Genres = await this.userRepository.findOne(request.params.id);
+        const status = typeof genres === "undefined" ? 400 : 200;
+        response.status(status);
+        response.json(genres);
         return this.userRepository.findOne(request.params.id);
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
+        try {
+            const genres: Genres = await this.userRepository.save(request.body);
+            response.status(200);
+            response.json(genres);
+            return this.userRepository.save(request.body);
+          } catch (error) {
+            response.status(400);
+            response.json(error.message);
+          }
     }
 
     async fix(request: Request, response: Response, next: NextFunction) {
-        this.userRepository.update(request.params.id,request.body);
+        await this.userRepository.update(request.params.id,request.body);
+        response.status(200);
+        response.json(response);
         return this.userRepository.findOne(request.params.id);
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
+        const genresToRemove = await this.userRepository.findOne(request.params.id);
         console.log(request.params.id);
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
+        console.log(genresToRemove);
+        await this.userRepository.remove(genresToRemove);
     }
 
 }
--- src/controller/LeftoversController.ts
+++ src/controller/LeftoversController.ts
@@ -7,26 +7,45 @@ export class LeftoversController {
     private userRepository = getRepository(Leftovers);
 
     async all(request: Request, response: Response, next: NextFunction) {
+        const leftovers: Leftovers[] = await this.userRepository.find();
+        const status = typeof leftovers === "undefined" ? 400 : 200;
+        response.status(status);
+        response.json(leftovers);
         return this.userRepository.find();
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
+        const leftovers: Leftovers = await this.userRepository.findOne(request.params.id);
+        const status = typeof leftovers === "undefined" ? 400 : 200;
+        response.status(status);
+        response.json(leftovers);
         return this.userRepository.findOne(request.params.id);
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
+        try {
+            const leftovers: Leftovers = await this.userRepository.save(request.body);
+            response.status(200);
+            response.json(leftovers);
+            return this.userRepository.save(request.body);
+          } catch (error) {
+            response.status(400);
+            response.json(error.message);
+          }
     }
 
     async fix(request: Request, response: Response, next: NextFunction) {
-        this.userRepository.update(request.params.id,request.body);
+        await this.userRepository.update(request.params.id,request.body);
+        response.status(200);
+        response.json(response);
         return this.userRepository.findOne(request.params.id);
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
+        const leftoversToRemove = await this.userRepository.findOne(request.params.id);
         console.log(request.params.id);
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
+        console.log(leftoversToRemove);
+        await this.userRepository.remove(leftoversToRemove);
     }
 
 }
